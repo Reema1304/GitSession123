de55f7bf492f0329e5ae445b915369e11a5d26ad
Reema1304 <reemateltumbade1@gmail.com>
2022-11-12 16:53:17 +0530

    printed array element of class PrintArray1

--- src/main/java/com/packagea/PrintArray1.java
+++ src/main/java/com/packagea/PrintArray1.java
@@ -1,5 +1,15 @@
 package com.packagea;
 
 public class PrintArray1 {
+	public static void main(String[] args) {
+		int arr[]= {1,2,4,5,6,7};
+		System.out.println("Printing Array");
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+
+	
 
 }
